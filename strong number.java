e10a8daab63794e562ecd1de96913064d274da6c
mdsopikul572-coder <mdsopikul572@gmail.com>
2026-08-23 19:22:36 +0600

    Update strong number.java

--- strong number.java
+++ strong number.java
@@ -1,8 +1,30 @@
-public class Main {
-    public static void main(String[] args) {
-        String name = "Sopikul";
+import java.util.Scanner;
 
-        System.out.println("Name: " + name);
-        System.out.println("Length: " + name.length());
+public class StrongNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        int original = num;
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            int fact = 1;
+
+            for (int i = 1; i <= digit; i++) {
+                fact = fact * i;
+            }
+
+            sum = sum + fact;
+            num = num / 10;
+        }
+
+        if (sum == original)
+            System.out.println(original + " is a Strong Number");
+        else
+            System.out.println(original + " is not a Strong Number");
     }
 }
